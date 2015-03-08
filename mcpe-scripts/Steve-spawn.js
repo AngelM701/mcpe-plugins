8eee364ad1e97c170ee93a1d7820c2f9032c98ea
Angel Maya <thecrazymariachi@gmail.com>
2015-03-08 14:00:58 -0500

    remember this is free to use :)

---
+++ mcpe-scripts/Steve-spawn.js
@@ -0,0 +1,11 @@
+function useItem(x, y, z, iteid, blockId, side) {// this hole code will spawn steve by tapping the ground
+        if(itemId == 0) {
+                   var custom = Level.spawnMob(x,y+1,z,11"mob/char.png");
+                   Entity.setRenderType(custom, 3)
+                  clientMessae("Steve Joined The Game");
+      }
+}
+
+function attackHook(attacker,victim) {// this code will remove entities by tapping on them this is only intended for steve!
+Entity.remove(victim);
+}
