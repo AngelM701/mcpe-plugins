b86b6ed5b28619cf0d8c566c5c636b56a480ac93
Angel Maya <thecrazymariachi@gmail.com>
2015-03-09 16:39:54 -0500

    fixed: line #1, #3 and #7

--- mcpe-scripts/Steve-spawn.js
+++ mcpe-scripts/Steve-spawn.js
@@ -1,13 +1,14 @@
-function useItem (x, y, z, iteid, blockId, side)
+function useItem (x, y, z, itemId, blockId, side)
 {// this hole code will spawn steve by tapping the ground
-        if(itemId == 218)
+        if(itemId==218)
         {//you need to tap on the ground with a stick
-                   var custom = Level.spawnMob(x,y+1,z,11,"mob/char.png");
-                   Entity.setRenderType(custom, 3)
-                  clientMessae("Steve Joined The Game");
+                   var custom = Level.spawnMob(x,y+1,z,11,"mob/char.png");// char.png can be your skin too :)
+                   Entity.setRenderType(custom, 3);
+                  clientMessage("Steve Joined The Game");
       }
 }
 
 function attackHook(attacker,victim) {// this code will remove entities by tapping on them this is only intended for steve!
 Entity.remove(victim);
+clientMessage("Steve Joined The Game");
 }
